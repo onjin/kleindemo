fed5541a16a3ed724ec6ac026080e5630fc8ef1c
Kevin Turner <keturn@keturn.net>
2012-09-05 20:27:37 -0700

    serve static HTML for the front page and a static directory with CSS.

--- kleindemo/main.py
+++ kleindemo/main.py
@@ -1,8 +1,18 @@
 from klein import run, route
+from twisted.web.static import File
+
+@route('/static/')
+def static(request):
+    return File("./static")
+
+
 
 @route('/')
 def home(request):
-    return "Hello World!"
+    with file('./static/home.html') as f:
+        return f.read()
+
+
 
 if __name__ == '__main__':
     run("localhost", 8081)
